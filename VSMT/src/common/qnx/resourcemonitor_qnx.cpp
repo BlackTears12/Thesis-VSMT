f074a7d5523e0bf6d66e3cf9d48b2fcbc7de59e8
BlackTears12 <szombati.balint@gmail.com>
2024-11-12 10:28:23 +0100

    qnx first try

---
+++ VSMT/src/common/qnx/resourcemonitor_qnx.cpp
@@ -0,0 +1,227 @@
+#include "resourcemonitor_qnx.hpp"
+#include <QStorageInfo>
+
+namespace metrics::monitor
+{
+
+ResourceMonitor<Platform::QNX>::CpuMonitor::CpuMonitor()
+{
+    info = gatherProcessorInfo();
+    previousIdleTimes.resize(info.cores);
+    previousTotalTimes.resize(info.cores);
+}
+
+CpuInfo ResourceMonitor<Platform::QNX>::CpuMonitor::gatherProcessorInfo()
+{
+    CpuInfo info;
+    /*try {
+        util::SysFileParser fileParser("/proc/cpuinfo");
+        QString modelFullStr = fileParser.valueAt("model name").value_or(QStringList{"Not Available@Not Available"}).join(" ");
+        auto modelIndx = modelFullStr.lastIndexOf('@');
+        info.model = modelFullStr.left(modelIndx-1);
+        info.speed = modelFullStr.right(modelFullStr.size()-modelIndx-2);
+        // the last entry of a processor core is at the "processor" key
+        info.cores = fileParser.valueAt("processor").value_or(QStringList{"0"})[0].toInt()+1;
+    }
+    catch(util::FileNotFoundException &e) {
+        qDebug() << "File not found: " << e.fileName();
+    }*/
+    return info;
+}
+
+vector<double> ResourceMonitor<Platform::QNX>::CpuMonitor::gatherCoreLoads()
+{
+    vector<double> loads;
+    QFile file("/proc/stat");
+    if(!file.open(QIODevice::ReadOnly)) {
+        qDebug() << "Cannot open file";
+        return loads;
+    }
+    ///discard the first line containing the summed values
+    (void)file.readLine();
+
+    loads.resize(info.cores);
+    vector<unsigned int> data (9);
+    ///read core_n lines and calculate the load
+    for(int coreIdx = 0; coreIdx < info.cores; coreIdx++) {
+        /* CPU nice systen idle iowait irq softirq steal guest guest_nice */
+        auto parts = file.readLine().split(' ');
+        parts.removeFirst();
+        std::transform(parts.begin(),parts.end(),data.begin(),
+                       [](const QByteArray& data){ return data.toUInt(); });
+        loads[coreIdx] = calculateLoadForCore(data,coreIdx);
+    }
+    return loads;
+}
+
+double ResourceMonitor<Platform::QNX>::CpuMonitor::calculateLoadForCore(const vector<unsigned int> &data, unsigned int coreIdx)
+{
+    // idle = idle + iowait
+    unsigned int idleTime = data.at(3) + data.at(4);
+    unsigned int totalTime = std::reduce(data.begin(),data.end());
+
+    unsigned int idleDelta = idleTime - previousIdleTimes[coreIdx];
+    unsigned int totalDelta = totalTime - previousTotalTimes[coreIdx];
+
+    previousTotalTimes[coreIdx] = totalTime;
+    previousIdleTimes[coreIdx] = idleTime;
+
+    // if for some reason the core wasnt used since the prev measurement
+    if(totalDelta <= 0)
+        return 0;
+
+    return util::percentof(totalDelta-idleDelta,totalDelta);
+}
+
+
+vector<NetworkInterfaceInfo> ResourceMonitor<Platform::QNX>::NetworkMonitor::gatherInfo()
+{
+    updateCurrentLoads();
+    vector<NetworkInterfaceInfo> infos;
+    const auto &interfaces = QNetworkInterface::allInterfaces();
+    infos.reserve(interfaces.size());
+    for(auto &interface : interfaces) {
+        infos.push_back(gatherNetworkInterfaceLoad(interface));
+    }
+    return infos;
+}
+
+NetworkInterfaceInfo ResourceMonitor<Platform::QNX>::
+    NetworkMonitor::gatherNetworkInterfaceLoad(const QNetworkInterface &interface)
+{
+    auto prev = previousLoads.value(interface.name(),{0,0});
+    auto current = currentLoads.value(interface.name(),prev);
+
+    return {
+        interface.humanReadableName(),
+        interface.type(),
+        current.rx - prev.rx,
+        current.tx - prev.tx
+    };
+}
+
+void ResourceMonitor<Platform::QNX>::NetworkMonitor::updateCurrentLoads()
+{
+    previousLoads = currentLoads;
+    currentLoads.clear();
+    QFile file("/proc/net/dev");
+    if(!file.open(QIODevice::ReadOnly)) {
+        qDebug() << "cannot open /proc/net/dev";
+        return;
+    }
+    file.readLine();
+    file.readLine();
+    QByteArray line = file.readLine();
+    while(!line.isEmpty()) {
+        auto ln = QString{line}.trimmed();
+        line = file.readLine();
+        auto parts = ln.split(':');
+        if(parts.size() < 2) {
+            continue;
+        }
+        auto values = parts[1].trimmed().split(' ');
+        values.removeAll(QString{""});
+        if(values.size() < 16) {
+            continue;
+        }
+
+        currentLoads.insert(parts[0].trimmed(),{
+            .rx = values[0].trimmed().toInt(),
+            .tx = values[8].trimmed().toInt(),
+        });
+    }
+}
+
+unique_ptr<SystemInfo> ResourceMonitor<Platform::QNX>::gatherSysInfo()
+{
+    if(!sysInfoCache) {
+        sysInfoCache = std::make_unique<SystemInfo>();
+        sysInfoCache->platform = QString("%1 %2").arg(QSysInfo::kernelType(),QSysInfo::currentCpuArchitecture());
+        sysInfoCache->distribution = QSysInfo::prettyProductName();
+        sysInfoCache->cpuInfo = cpuMonitor.getInfo();
+    }
+    return std::unique_ptr<SystemInfo>(new SystemInfo(*sysInfoCache));
+}
+
+
+MemoryInfo ResourceMonitor<Platform::QNX>::gatherMemoryInfo() const
+{
+    MemoryInfo info;
+    /*try {
+        util::SysFileParser fileParser("/proc/meminfo");
+        info.total = fileParser.valueAt("MemTotal").value_or(QStringList{"100"})[0].toInt();
+        auto available = fileParser.valueAt("MemAvailable").value_or(QStringList{"0"})[0].toInt();
+        info.used = info.total - available;
+    } catch(util::FileNotFoundException &e) {
+        qDebug() << "File not found: " << e.fileName();
+    }*/
+    return info;
+}
+
+ProcessInfo ResourceMonitor<Platform::QNX>::parsePSCommandRow(const QString &line) const
+{
+    auto parts = line.split(' ');
+    parts.removeAll("");
+
+    return ProcessInfo {
+        (pid_t)parts[0].toUInt(),
+        ProcessInfo::StrToStatus(parts[1]),
+        parts[2].toDouble(),
+        parts[3].toDouble(),
+        parts[4],
+        parts[5]
+    };
+}
+
+vector<ProcessInfo> ResourceMonitor<Platform::QNX>::gatherProcessInfo() const
+{
+    vector<ProcessInfo> infos;
+    /*try {
+        QString output = util::cmd::exec("ps",{"--no-headings","-w","-a","-e","-o",
+                                              "pid,state,pcpu,pmem,comm,args"});
+        QTextStream stream(&output);
+        while(!stream.atEnd()) {
+            infos.push_back(parsePSCommandRow(stream.readLine()));
+        }
+    } catch(util::cmd::ExecutionError &e) {
+        qDebug() << "Cannot exec ps";
+    }*/
+    return infos;
+}
+
+unique_ptr<RuntimeMetric> ResourceMonitor<Platform::QNX>::gatherRuntimeMetric()
+{
+    return std::make_unique<RuntimeMetric>(
+        cpuMonitor.gatherCoreLoads(),
+        gatherMemoryInfo(),
+        gatherProcessInfo(),
+        gatherStorageInfo(),
+        gatherCanDevInfo(),
+        networkMonitor.gatherInfo()
+    );
+}
+
+vector<StorageInfo> ResourceMonitor<Platform::QNX>::gatherStorageInfo() const
+{
+    vector<StorageInfo> infos;
+    auto mountedVolumes = QStorageInfo::mountedVolumes();
+    for(const auto &storage : mountedVolumes) {
+        if(!storage.isRoot() || !storage.isReady() || storage.isReadOnly())
+            continue;
+        infos.push_back(StorageInfo {
+            storage.displayName(),
+            storage.fileSystemType(),
+            storage.device(),
+            util::byteToMb(storage.bytesTotal()),
+            util::byteToMb(storage.bytesAvailable())
+        });
+    }
+    return infos;
+}
+
+vector<CanBusDeviceInfo> ResourceMonitor<Platform::QNX>::gatherCanDevInfo() const
+{
+    return {};
+}
+
+}
---
+++ VSMT/src/common/qnx/resourcemonitor_qnx.hpp
@@ -0,0 +1,64 @@
+#ifndef RESOURCEMONITOR_LINUX_HPP
+#define RESOURCEMONITOR_LINUX_HPP
+
+#include "resourcemonitor.hpp"
+
+namespace metrics::monitor
+{
+
+template<>
+class ResourceMonitor<Platform::QNX>
+{
+    class CpuMonitor
+    {
+    public:
+        CpuMonitor();
+
+        const CpuInfo& getInfo() const { return info; }
+        vector<double> gatherCoreLoads();
+    private:
+        CpuInfo gatherProcessorInfo();
+        double calculateLoadForCore(const vector<unsigned int> &data,unsigned int coreIdx);
+
+        CpuInfo info;
+        QVector<unsigned int> previousIdleTimes;
+        QVector<unsigned int> previousTotalTimes;
+    };
+    class NetworkMonitor
+    {
+    public:
+        NetworkMonitor() {}
+        vector<NetworkInterfaceInfo> gatherInfo();
+    private:
+        NetworkInterfaceInfo gatherNetworkInterfaceLoad(const QNetworkInterface &interface);
+        void updateCurrentLoads();
+
+        struct LoadSnapShot
+        {
+            qint64 rx;
+            qint64 tx;
+        };
+
+        QMap<QString,LoadSnapShot> currentLoads;
+        QMap<QString,LoadSnapShot> previousLoads;
+    };
+public:
+    explicit ResourceMonitor() { };
+
+    unique_ptr<SystemInfo> gatherSysInfo();
+    unique_ptr<RuntimeMetric> gatherRuntimeMetric();
+private:
+    MemoryInfo gatherMemoryInfo() const;
+    vector<ProcessInfo> gatherProcessInfo() const;
+    ProcessInfo parsePSCommandRow(const QString &line) const;
+    vector<StorageInfo> gatherStorageInfo() const;
+    vector<CanBusDeviceInfo> gatherCanDevInfo() const;
+
+    unique_ptr<SystemInfo> sysInfoCache;
+    CpuMonitor cpuMonitor;
+    NetworkMonitor networkMonitor;
+};
+
+}
+
+#endif // RESOURCEMONITOR_LINUX_HPP
