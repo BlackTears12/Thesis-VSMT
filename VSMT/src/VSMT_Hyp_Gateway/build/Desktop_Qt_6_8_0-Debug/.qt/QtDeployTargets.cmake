d0166cf0d9e751ceaed99f66650a8930900f7afe
BlackTears12 <szombati.balint@gmail.com>
2024-11-28 10:38:07 +0100

    fix merge

--- VSMT/src/VSMT_Hyp_Gateway/build/Desktop_Qt_6_8_0-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_VSMT_Hyp_Gateway_FILE /home/ext-szombatb/thesis/VSMT/src/VSMT_Hyp_Gateway/build/Desktop_Qt_6_8_0-Debug/VSMT_Hyp_Gateway)
-set(__QT_DEPLOY_TARGET_VSMT_Hyp_Gateway_TYPE EXECUTABLE)
